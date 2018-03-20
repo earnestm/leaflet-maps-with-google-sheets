516548512a4a8ce714bb4c0c8b46e1f184758ec4
Luc Steinberg <luc.steinberg@gmail.com>
2018-03-20 14:35:41 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1JFnfaKJIW-J5_cJjzb2S-obncfoxjAwTF944nIQNDzc/edit#gid=0';
